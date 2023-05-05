c1f77b2a9d1eccdf1c9a4712a614e6c9d448ebc8
Seavhongchea <132644249+Seavhongchea@users.noreply.github.com>
2023-05-05 13:18:50 +0700

    Create decryption(all).py

---
+++ decryption(all).py
@@ -0,0 +1,49 @@
+# The Decryption Function
+def cipher_decrypt(ciphertext, key):
+
+    decrypted = ""
+
+    for c in ciphertext:
+
+        if c.isupper(): 
+
+            c_index = ord(c) - ord('A')
+
+            # shift the current character to left by key positions to get its original position
+            c_og_pos = (c_index - key) % 26 + ord('A')
+
+            c_og = chr(c_og_pos)
+
+            decrypted += c_og
+
+        elif c.islower(): 
+
+            c_index = ord(c) - ord('a') 
+
+            c_og_pos = (c_index - key) % 26 + ord('a')
+
+            c_og = chr(c_og_pos)
+
+            decrypted += c_og
+
+        elif c.isdigit():
+
+            # if it's a number,shift its actual value 
+            c_og = (int(c) - key) % 10
+
+            decrypted += str(c_og)
+
+        else:
+
+            # if its neither alphabetical nor a number, just leave it like that
+            decrypted += c
+
+    return decrypted
+
+
+for i in range(0,26):
+
+    plain_text = cipher_decrypt(ciphertext, i)
+
+    print("For key {}, decrypted text: {}".format(i, plain_text))
+
